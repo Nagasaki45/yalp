8ca47faf9bb8da6e1892baa6db4d1191f1b1258f
Tom Gurion <nagasaki45@gmail.com>
2019-04-18 17:15:23 +0100

    Look mum, I'm a package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nagasaki45/yalp",
-    py_modules=["yalp"]
+    packages=["yalp"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
---
+++ yalp/__init__.py
@@ -0,0 +1,1 @@
+from .yalp import parse_log, parse_log_message, parse_log_metadata
